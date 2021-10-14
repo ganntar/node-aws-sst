e947ecca56adf8b032ab7111926ebefc3a60995e
Alexandre Barbosa <ganntar@gmail.com>
2021-10-14 14:51:52 -0300

    Adding a storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,22 @@
+import * as sst from "@serverless-stack/resources";
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as cdk from '@aws-cdk/core';
+
+export default class StorageStack extends sst.Stack {
+  // Public reference to the table
+  table;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    this.placeTable = new sst.Table(this, 'PlaceTable', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        placeId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: { partitionKey: "userId", sortKey: "placeId" },
+    });
+  }
+}
